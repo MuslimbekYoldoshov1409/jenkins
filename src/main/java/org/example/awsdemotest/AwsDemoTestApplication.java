d1c40a4b649c1b3a37d37ebfffccba4a583c1a01
Muslimbek Yo'ldoshov <muslimyoldoshov41@gmail.com>
2024-01-10 14:34:43 +0500

    initial commit

---
+++ src/main/java/org/example/awsdemotest/AwsDemoTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.awsdemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDemoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/awsdemotest/AwsDemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsdemotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
